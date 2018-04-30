e4ccf3dff92729c2e17a36433cfd62904845bea4
Spencer Young <syoung@mailbox.org>
2018-05-01 01:01:21 +0200

    Scheduler durch resilientere Alternative ersetzt

--- binaries/DHT.py
+++ binaries/DHT.py
@@ -1,59 +1,48 @@
 #!/usr/bin/python3.5
-import schedule
-from time import localtime, strftime, clock, sleep
+from apscheduler.schedulers.blocking import BlockingScheduler
+from time import strftime
 from Adafruit_DHT import read_retry, AM2302
 from numpy import exp, log
-from os import popen, path, remove
+from datetime import datetime
 
-import sys
-import signal
+maxProbeTries = 3
+dbConnection = None #TODO: Datenbank einrichten!
 
 def probe():
-	csvFilename = path.join(dir, strftime('%y-%m-%d.csv'))
-
-	# Try to grab a sensor reading.  Use the read_retry method which will retry up
-	# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-	humidity1, temperature1 = read_retry(AM2302, 2)
-
-	humidity2, temperature2 = read_retry(AM2302, 2)
-
-	#TODO Do-While-Schleife für Validierung einführen
-	print(popen('tail -n 1 ' + csvFilename).read())
-	if  abs(temperature1 - temperature2) >= 1 or abs(humidity1 - humidity2) >= 2:
-        	line = popen('tail -n 1 ' + csvFilename).read()
-	        values=line.split()
-	        if abs( float(values[1]) - temperature1 ) >= 2:
-        	        temperature = temperature2
-                	humidity = humidity2
-	        elif abs( float(values[1]) - temperature2 ) >= 2:
-        	        temperature = temperature1
-                	humidity = humidity1
-	        elif abs( float(values[2]) - humidity1 ) >= 5:
-        	        temperature = temperature2
-                	humidity = humidity2
-	        elif abs( float(values[2]) - humidity2 ) >= 5:
-        	        temperature = temperature1
-                	humidity = humidity1
-	        else:
-        	        print('Error! {0:0.1f} {1:0.1f} {2:0.1f} {3:0.1f}'.format(temperature1, temperature2, humidity1, humidity2))
-                	#sys.exit(67)
+	probeTryCount=0
+	print("%s\t" % datetime.time(datetime.now()), end='')
+	#Do-While-Schleife für die Validierung
+	try:
+		while True:
+			humidity1, temperature1 = read_retry(AM2302, 2) # Try to grab a sensor reading. Use the read_retry method which will retry up
+			humidity2, temperature2 = read_retry(AM2302, 2) # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+			probeTryCount += 1
+			if abs(temperature1 - temperature2) < 1 and abs(humidity1 - humidity2) < 2:
+				break
+			if probeTryCount >= maxProbeTries:
+				raise ResourceWarning('Maximum Probe Tries of ' + maxProbeTries + ' exceeded, please check sensor availability.')
+	except ResourceWarning as w:
+		print(w)
 	else:
-	        humidity = (humidity1+humidity2)/2
-        	temperature = (temperature1+temperature2)/2
-	        dewpt = (-3928.5/(log(humidity*exp(-3928.5/(temperature+231.667)))-4.60517))-231.667
+		humidity = (humidity1+humidity2)/2
+		temperature = (temperature1+temperature2)/2
 
-	with open(csvFilename , "a") as file:
-        	file.write(strftime("%H:%M ") + '{0:0.2f} {1:0.2f} {2:0.2f}\n'.format(temperature, humidity, dewpt))
+		#Taupunktberechnung, Formel anhand von TODO: Quelle angeben!  aufgebaut und per CAS gekürzt, sodass die Berechnung in einem Schritt erfolgt.
+		dewpt = (-3928.5/(log(humidity*exp(-3928.5/(temperature+231.667)))-4.60517)
+			)-231.667
+
+		print('{0:0.2f} {1:0.2f} {2:0.2f}'.format(temperature, humidity, dewpt)) # TODO: In Datenbank schreiben!
 
 def plot():
 	print("Jetzt ist die Plot-Funktion auszuführen.")
 
-dir = path.join(path.dirname(path.abspath(__file__)), path.pardir, 'csvs')
+if __name__ == '__main__':
+	scheduler = BlockingScheduler()
+	scheduler.add_executor('threadpool')
+	scheduler.add_job(probe, 'cron', second=5)
+	scheduler.add_job(plot, 'cron', minute='*/5')
 
-schedule.every().minute.do(probe)
-schedule.every(5).minutes.do(plot)
-
-schedule.run_all()
-while 1:
-	schedule.run_pending()
-	sleep(5)
+	try:
+		scheduler.start()
+	except (KeyboardInterrupt, SystemExit):
+		pass
