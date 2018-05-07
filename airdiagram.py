a3c78ab5ecc6fd71a3b2d42d977dd7b6396dbcea
Spencer Young <syoung@mailbox.org>
2018-05-08 00:28:09 +0200

    Toleranz-Verhalten und Errorhandling

--- airdiagram.py
+++ airdiagram.py
@@ -5,13 +5,14 @@ from time import strftime
 from Adafruit_DHT import read_retry, AM2302
 from numpy import exp, log
 from datetime import datetime
-from sqlite3 import connect as sql_connect
+from sqlite3 import OperationalError, connect as sql_connect
 from sys import argv, exit
 from getopt import getopt, GetoptError
 from os import path
 from paramiko import SSHClient
-from paramiko.ssh_exception import SSHException
-from scp import SCPClient
+from paramiko.ssh_exception import SSHException, AuthenticationException, NoValidConnectionsError
+from scp import SCPClient, SCPException
+from socket import gaierror
 
 maxProbeTries = 3
 dbConnection = None
@@ -44,48 +45,63 @@ def probe():
 			cur = dbConnection.cursor()
 			cur.execute("insert into Probes values ( strftime('%s','now'), ?, ?, ?)", (temperature, humidity, dewpt))
 
-def plot(scp, remotepath):
+def plot(scp, remotepath, tolerant):
 	htmlFileName = 'scatter.html'
 	print("Jetzt ist die Plot-Funktion auszuführen.")
 
 	# Push auf gewählten Server per SCP
-	scp.put(htmlFileName, remote_path = remotepath)
+	try:
+		scp.put(htmlFileName, remote_path = remotepath)
+	except SCPException as e:
+		print("Fehler beim Übertragen der Datei, möglicherweise ist der angegebene Pfad fehlerhaft. Fehlermeldung: %s" % e)
+		if not tolerant:
+			exit(31)
 
 if __name__ == '__main__':
-	dbFilePath = './airdata.db' # Standardwert
+	dbFilePath = './data.db' # Standardwert
 	probeCronTabExpression = ''
 	plotCronTabExpression = ''
-	hostname = 'localhost'
-	username = 'pi'
-	remotepath = 'public-html/scatter.html'
+	hostname = 'localhost' # Dummy-Standardwert
+	tolerant = False # reagiere auf Fehler im Standardfall stets mit exit.
+	username = 'pi' # Dummy-Standardwert
+	remotepath = '.' # Standardwert
 
 	# Parsen der übergebenen Parameter
 	try:
-		opts, args = getopt(argv[1:],"hd:H:p:P:u:",["help", "dbfile=", "hostname=", "probeinterval=","plotinterval=", "username="])
+		opts, args = getopt(argv[1:],"hd:H:p:P:r:tu:",["help", "dbfile=", "hostname=", "probecronexpr=","plotcronexpr=", "remotepath=", "tolerant", "username="])
 	except GetoptError:
-		print("Usage:\n%s [-h] [-d <dbfile>] [-H <hostname>] [-p <probeinterval>] [-P <plotinterval>] [-u <username>]" % argv[0])
+		print("Usage:\n%s [-h] [-d <dbfile>] [-H <hostname>] [-p <probecronexpr>] [-P <plotcronexpr>] [-r <remotepath>] [-t] [-u <username>]" % argv[0])
 		exit(42)
 
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
-			print("Usage:\n%s [-h] [-d <dbfile>] [-H <hostname>] [-p <probeinterval>] [-P <plotinterval>] [-u <username>]" % argv[0])
+			print("Usage:\n%s [-h] [-d <dbfile>] [-H <hostname>] [-p '<probecronexpr>'] [-P '<plotcronexpr>'] [-r <remotepath>] [-t] [-u <username>]" % argv[0])
 			exit(0)
 		elif opt in ("-d", "--dbfile"):
 			dbFilePath = arg
 		elif opt in ("-H", "--hostname"):
 			hostname = arg
-		elif opt in ("-p", "--probeinterval"):
+		elif opt in ("-p", "--probecronexpr"):
 			probeCronTabExpression = arg
-		elif opt in ("-P", "--plotinterval"):
+		elif opt in ("-P", "--plotcronexpr"):
 			plotCronTabExpression = arg
+		elif opt in ("-r", "--remotepath"):
+			remotepath = arg
+		elif opt in ("-t", "--tolerant"):
+			tolerant = True
 		elif opt in ("-u", "--username"):
 			username = arg
 
-	dbConnection = sql_connect(dbFilePath, check_same_thread=False)
-	# Falls die benötigte Tabelle in der Datenbank noch nicht vorhanden ist → Anlegen
-	with dbConnection:
-		cur = dbConnection.cursor()
-		cur.execute("create table if not exists Probes (Timestamp int, Temperature float, Humidity float, DewPoint float);")
+	try:
+		dbConnection = sql_connect(dbFilePath, check_same_thread=False)
+
+		# Falls die benötigte Tabelle in der Datenbank noch nicht vorhanden ist → Anlegen
+		with dbConnection:
+			cur = dbConnection.cursor()
+			cur.execute("create table if not exists Probes (Timestamp int, Temperature float, Humidity float, DewPoint float);")
+	except OperationalError as e:
+		print("Schwerwiegender Datenbank-Fehler:", e.args[0])
+		exit(26)
 
 	ssh = SSHClient()
 	ssh.load_system_host_keys()
@@ -98,9 +114,19 @@ if __name__ == '__main__':
 
 	try:
 		ssh.connect(hostname = hostname, username = username)
-	except SSHException:
-		print("Der Server „%s“ konnte nicht in der Datei known_hosts gefunden werden, Abbruch." % hostname)
+	except SSHException as e:
+		print("Der Hostname „%s“ konnte nicht in der Datei known_hosts gefunden werden, Abbruch. Fehlermeldung: " % (hostname, e))
 		exit(13)
+	except AuthenticationException as e:
+		print("Authentifizierungsproblem: %s" % e.args[0])
+		exit(23)
+	except NoValidConnectionsError as e:
+		print("Die Verbindung zu „%s@%s“ ist fehlgeschlagen: %s" % (username, hostname, e.args[1]))
+		exit(29)
+	except gaierror:
+		print("Der Hostname „%s“ konnte nicht gefunden werden." % hostname)
+		exit(17)
+
 	scp = SCPClient(ssh.get_transport())
 
 	scheduler = BlockingScheduler()
@@ -112,13 +138,13 @@ if __name__ == '__main__':
 		scheduler.add_job(probe, CronTrigger.from_crontab(probeCronTabExpression))
 
 	if (plotCronTabExpression == ''):
-		scheduler.add_job(plot, 'cron', args=[scp, remotepath], minute='*/5', second=30) # verwende Standard-Intervall, falls keine Cron-Tab-Expr. gesetzt
+		scheduler.add_job(plot, 'cron', args=[scp, remotepath, tolerant], minute='*/1', second=30) # verwende Standard-Intervall, falls keine Cron-Tab-Expr. gesetzt
 	else:
-		scheduler.add_job(probe, CronTrigger.from_crontab(plotCronTabExpression))
+		scheduler.add_job(plot, CronTrigger.from_crontab(plotCronTabExpression), args=[scp, remotepath, tolerant])
 
 
 	#probe() #Zum Testen! Später entfernen
-	plot(scp, remotepath)
+	plot(scp, remotepath, tolerant)
 	try:
 		scheduler.start()
 	except (KeyboardInterrupt, SystemExit):
